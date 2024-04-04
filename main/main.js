5fe42e22460685829125638e32b1d7f8d50d8d78
Lucas Sponton <lucas.sponton6@gmail.com>
2024-04-04 20:09:26 -0300

    Aplique correciones que me dio el tutor para la 2da pre-entrega.

--- main/main.js
+++ main/main.js
@@ -1,237 +1,118 @@
+
 let totalCarrito = 0
 
-class Helado {
-    constructor(tamaño, precio) {
-        this.tamaño = tamaño
-        this.precio = precio
-    }
-}
-let vainilla = "Vainilla"
-let chocolate = "Chocolate"
-let fresa = "Fresa"
-let menta = "Menta"
-let limón = "Limón"
-let café = "Café"
-let coco = "Coco"
-let banana = "Banana"
-let frambuesa = "Frambuesa"
-let mango = "Mango"
-let tiramisú = "Tiramisú"
-let dulceDeLeche = "DulceDeLeche"
-let piña = "Piña"
-let maracuyá = "Maracuyá"
+const producto = [{ id: 1, nombre: 'Cuarto kilo de helado', precio: 1200 },
+{ id: 2, nombre: 'Medio kilo de helado', precio: 1700 },
+{ id: 3, nombre: 'Un kilo de helado', precio: 2300 }]
 
-let cuarto = new Helado("Cuarto de kilo", 1200)
-let medio = new Helado("Medio kilo de helado", 1700)
-let kilo = new Helado("Kilo de helado", 2300)
+const sabores = [{ id: 1, nombre: "Vainilla" },
+{ id: 2, nombre: "Chocolate" },
+{ id: 3, nombre: 'Fresa' },
+{ id: 4, nombre: 'Menta' },
+{ id: 5, nombre: 'Limón' },
+{ id: 6, nombre: 'Café' },
+{ id: 7, nombre: 'Coco' },
+{ id: 8, nombre: 'Banana' },
+{ id: 9, nombre: 'Frambuesa' },
+{ id: 10, nombre: 'Mango' },
+{ id: 11, nombre: 'Tiramisú' },
+{ id: 12, nombre: 'DulceDeLeche' },
+{ id: 13, nombre: 'Piña' },
+{ id: 14, nombre: 'Maracuyá' },
+]
 
-const arrayPedido = [cuarto, medio, kilo]
-const arraySabor = [vainilla, chocolate, fresa, menta, limón, café, coco, banana, frambuesa, mango, tiramisú, dulceDeLeche, piña, maracuyá]
+const pedido = producto.map((el) => {
+    return el.id + '- ' + el.nombre + '; ' + el.precio + '\n'
+})
 
-function elegirSabor() {
-    let sabores = parseInt(prompt(`Eliga sus sabores por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
+const eleccion = sabores.map((el) => {
+    return el.id + '- ' + el.nombre + '\n'
+})
 
-    while (sabores < 1 || sabores > 14) {
-        sabores = parseInt(prompt(`Eliga sus sabores por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
-    }
-    let gustos;
-    if (sabores == 1) {
-        gustos = arraySabor[0]
-    }
-    else if (sabores == 2) {
-        gustos = arraySabor[1]
-    }
-    else if (sabores == 3) {
-        gustos = arraySabor[2]
-    }
-    else if (sabores == 4) {
-        gustos = arraySabor[3]
-    }
-    else if (sabores == 5) {
-        gustos = arraySabor[4]
-    }
-    else if (sabores == 6) {
-        gustos = arraySabor[5]
-    }
-    else if (sabores == 7) {
-        gustos = arraySabor[6]
-    }
-    else if (sabores == 8) {
-        gustos = arraySabor[7]
-    }
-    else if (sabores == 9) {
-        gustos = arraySabor[8]
-    }
-    else if (sabores == 10) {
-        gustos = arraySabor[9]
-    }
-    else if (sabores == 11) {
-        gustos = arraySabor[10]
-    }
-    else if (sabores == 12) {
-        gustos = arraySabor[11]
-    }
-    else if (sabores == 13) {
-        gustos = arraySabor[12]
-    }
-    else if (sabores == 14) {
-        gustos = arraySabor[13]
-    }
-    
-    let sabores2 = parseInt(prompt(`Eliga otro sabor por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
-    while (sabores2 < 1 || sabores2 > 14){
-        sabores2 = parseInt(prompt(`Eliga otro sabor por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
-    }
-    let gustos2;
-    if (sabores2 == 1){
-        gustos2 = arraySabor[0]
-    }
-    else if (sabores2 == 2){
-        gustos2 = arraySabor[1]
-    }
-    else if (sabores2 == 3) {
-        gustos2 = arraySabor[2]
-    }
-    else if (sabores2 == 4) {
-        gustos2 = arraySabor[3]
-    }
-    else if (sabores2 == 5) {
-        gustos2 = arraySabor[4]
-    }
-    else if (sabores2 == 6) {
-        gustos2 = arraySabor[5]
-    }
-    else if (sabores2 == 7) {
-        gustos2 = arraySabor[6]
-    }
-    else if (sabores2 == 8) {
-        gustos2 = arraySabor[7]
-    }
-    else if (sabores2 == 9) {
-        gustos2 = arraySabor[8]
-    }
-    else if (sabores2 == 10) {
-        gustos2 = arraySabor[9]
-    }
-    else if (sabores2 == 11) {
-        gustos2 = arraySabor[10]
-    }
-    else if (sabores2 == 12) {
-        gustos2 = arraySabor[11]
-    }
-    else if (sabores2 == 13) {
-        gustos2 = arraySabor[12]
-    }
-    else if (sabores2 == 14) {
-        gustos2 = arraySabor[13]
-    }
+let total = [];
+let pedidoFinal = []
+let pedidoFinalPrecio = []
 
-    let sabores3 = parseInt(prompt(`Eliga otro sabor por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
-    while (sabores3 < 1 || sabores3 > 14){
-        sabores3 = parseInt(prompt(`Eliga otro sabor por su número: \n1- ${arraySabor[0]}\n2- ${arraySabor[1]}\n3- ${arraySabor[2]}\n4- ${arraySabor[3]}\n5- ${arraySabor[4]}\n6- ${arraySabor[5]}\n7- ${arraySabor[6]}\n8- ${arraySabor[7]}\n9- ${arraySabor[8]}\n10- ${arraySabor[9]}\n11- ${arraySabor[10]}\n12- ${arraySabor[11]}\n13- ${arraySabor[12]}\n14- ${arraySabor[13]}`))
+alert("Bienvenido a heladería Himalaya! \n A continuación puede hacer su pedido");
+function eleccionTamaño() {
+    let tamaño = parseInt(prompt("Elija el tamaño de su pedido por el número: \n" + pedido));
+    while (tamaño < 1 || tamaño > 3 || isNaN(tamaño)) {
+        alert("Por favor, indique su pedido por el número indicado!");
+        tamaño = parseInt(prompt("Elija el tamaño de su pedido por el número: \n" + pedido));
     }
-    let gustos3;
-    if (sabores3 == 1){
-        gustos3 = arraySabor[0]
-    }
-    else if (sabores3 == 2){
-        gustos3 = arraySabor[1]
-    }
-    else if (sabores3 == 3) {
-        gustos3 = arraySabor[2]
-    }
-    else if (sabores3 == 4) {
-        gustos3 = arraySabor[3]
-    }
-    else if (sabores3 == 5) {
-        gustos3 = arraySabor[4]
-    }
-    else if (sabores3 == 6) {
-        gustos3 = arraySabor[5]
-    }
-    else if (sabores3 == 7) {
-        gustos3 = arraySabor[6]
-    }
-    else if (sabores3 == 8) {
-        gustos3 = arraySabor[7]
-    }
-    else if (sabores3 == 9) {
-        gustos3 = arraySabor[8]
-    }
-    else if (sabores3 == 10) {
-        gustos3 = arraySabor[9]
-    }
-    else if (sabores3 == 11) {
-        gustos3 = arraySabor[10]
-    }
-    else if (sabores3 == 12) {
-        gustos3 = arraySabor[11]
-    }
-    else if (sabores3 == 13) {
-        gustos3 = arraySabor[12]
-    }
-    else if (sabores3 == 14) {
-        gustos3 = arraySabor[13]
-    }
-
-    let seleccion = confirm(`Usted eligió ${gustos}, ${gustos2} y ${gustos3}. ¿Es correcto?`)
-    if (seleccion == true) {
-        alert(`Perfecto, estos son sus sabores seleccionados: \n${gustos}, ${gustos2} y ${gustos3}.`)
-    }
-    else {
-        alert("Por favor, elija sus sabores!")
-        elegirSabor()
-    }
-}
-
-
-
-function comprar() {
-    let opciones = parseInt(prompt(`Pulse 1 si desea llevar un ${arrayPedido[0].tamaño} por $${arrayPedido[0].precio}\nPulse 2 si desea llevar ${arrayPedido[1].tamaño} por $${arrayPedido[1].precio}\nPulse 3 si desea llevar un ${arrayPedido[2].tamaño} por $${arrayPedido[2].precio}`))
-
-    while (opciones < 1 || opciones > 3) {
-        opciones = parseInt(prompt(`Pulse 1 si desea llevar un ${arrayPedido[0].tamaño} por $${arrayPedido[0].precio}\nPulse 2 si desea llevar ${arrayPedido[1].tamaño} por $${arrayPedido[1].precio}\nPulse 3 si desea llevar un ${arrayPedido[2].tamaño} por $${arrayPedido[2].precio}`))
-    }
-    let pedidoElegido;
-    if (opciones == 1) {
-        pedidoElegido = arrayPedido[0]
-        totalCarrito += arrayPedido[0].precio
-    }
-    else if (opciones == 2) {
-        pedidoElegido = arrayPedido[1]
-        totalCarrito += arrayPedido[1].precio
-    }
-    else if (opciones == 3) {
-        pedidoElegido = arrayPedido[2]
-        totalCarrito += arrayPedido[2].precio
-    }
-
-    let confirmacion = confirm(`Usted eligió ${pedidoElegido.tamaño} y sale $${pedidoElegido.precio}, desea agregarlo al carrito?`)
-    if (confirmacion == true) {
-        elegirSabor()
-        let confirmacion2 = confirm("El producto se agregó al carrito, ¿Desea comprar algo más?")
-        if (confirmacion2 == true) {
-            comprar()
-        } else {
-            let confirmacion3 = confirm(`El total es de ${totalCarrito}, ¿Desea pagar?`)
-            if (confirmacion3 == true) {
-                alert("Gracias por su compra")
-            } else {
-                alert("De acuerdo, se ha cancelado su pedido.")
-            }
+    if (tamaño >= 1 || tamaño <= 3) {
+        let seleccionTamaño = producto.find((el) => el.id === tamaño);
+        pedidoFinal.push(seleccionTamaño.nombre);
+        pedidoFinalPrecio.push(seleccionTamaño.precio)
+        alert("Usted eligio " + pedidoFinal + ' y sale: $' + pedidoFinalPrecio);
+        if (tamaño == 1 ) {
+            alert("En el cuarto entran hasta 3 sabores, si desea solo dos, o uno, seleccione el mismo sabor la cantidad de veces necesaria.")
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
         }
-    } else {
-        let confirmacion4 = confirm("¿Quiere seguir comprando?")
-        if (confirmacion4 == true) {
-            comprar()
-        } else {
-            alert("De acuerdo, esperamos su pedido!")
+        else if (tamaño == 2) {
+            alert("En el medio entran hasta 4 sabores, si desea solo tres o dos o uno, seleccione el mismo sabor la cantidad de veces necesaria.")
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
+        }
+        else if (tamaño == 3) {
+            alert("En el kilo entran hasta 5 sabores, si desea solo cuatro o tres o dos o uno, seleccione el mismo sabor la cantidad de veces necesarias.")
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
+            eleccionSabores()
         }
     }
 }
+eleccionTamaño()
 
-comprar()
+function cantidadSabores() {
+    if (tamaño == 1 ) {
+        alert("En el cuarto kilo de helado entran hasta 3 sabores, si desea solo dos, o uno, seleccione el mismo sabor la cantidad de veces necesaria.")
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+    }
+    else if (tamaño == 2) {
+        alert("En el medio kilo de helado entran hasta 4 sabores, si desea solo tres o dos o uno, seleccione el mismo sabor la cantidad de veces necesaria.")
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+    }
+    else if (tamaño == 3) {
+        alert("En el kilo de helado entran hasta 5 sabores, si desea solo cuatro o tres o dos o uno, seleccione el mismo sabor la cantidad de veces necesarias.")
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+        eleccionSabores()
+    }
+}
 
+function eleccionSabores() {
+    let eligio = parseInt(prompt("Elija un sabor por su número: \n" + eleccion));
+    while (isNaN(eligio) || eligio < 1 || eligio > 14) {
+        alert("Por favor, indique bien su sabor a elegir por su número.");
+        eligio = parseInt(prompt("Elija un sabor por su número: \n" + eleccion));
+    }
+    if (eligio >= 1 || eligio <= 14) {
+        let seleccion = sabores.find((el) => el.id === eligio);
+        total.push(" " + seleccion.nombre);
+    }
+}
+alert("Sus sabores son: \n" + total);
+
+let confirmacion = confirm(`Usted eligió ${pedidoFinal} y sale $${pedidoFinalPrecio}, ¿Desea pagar?`)
+if (confirmacion == true) {
+    alert("Gracias por su compra")
+} else {
+    alert("De acuerdo, se ha cancelado su pedido.")
+}
 
 
 
